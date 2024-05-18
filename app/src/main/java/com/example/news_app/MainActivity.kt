26c5a86197038b0dd68961bfb07abd3242cf7b6e
Gisel Caicedo <giselcaicedosoler@gmail.com>
2024-05-17 22:40:19 -0500

    Rehaciendo homepage para corregir errores de compilación

---
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.news_app
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.news_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
