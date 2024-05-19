236b5cb82fdea95c13007513bec094dbea50d53b
Gisel Caicedo <giselcaicedosoler@gmail.com>
2024-05-18 23:19:23 -0500

    Se termina de corregir el dashboard (titulares)

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.example.news_app
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.example.news_app.databinding.ActivityMainBinding
+import com.example.news_app.ui.dashboard.DashboardFragment
+import com.example.news_app.ui.home.HomeFragment
+import com.example.news_app.ui.notifications.NotificationsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val dashboardFragment = DashboardFragment()
+    private val homeFragment = HomeFragment()
+    private val notificationFragment = NotificationsFragment()
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    loadFragment(homeFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    loadFragment(dashboardFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    loadFragment(notificationFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigation: BottomNavigationView = findViewById(R.id.bar_navigate_menu)
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        loadFragment(homeFragment)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/example/news_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/news_app/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
